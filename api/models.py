dfc4409bdc6b5a4df8a2ecb71235769a792787a1
Pramod Gupta <pramod.g@otpxpress.in>
2020-10-19 11:26:55 +0530

    All

---
+++ api/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
+
+
+class User(AbstractUser):
+    username = models.CharField(blank=True, null=True)
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+
+
+    def __str__(self):
+        return "{}".format(self.email)
+
+        
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
+    title = models.CharField(max_length=10, null=True, blank=True)
+    dob = models.DateField(null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    country = models.CharField(max_length=50, null=True, blank=True)
+    city = models.CharField(max_length=50, null=True, blank=True)
+    zip = models.CharField(max_length=6, null=True, blank=True)
